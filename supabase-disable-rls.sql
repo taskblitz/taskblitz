7118597ff0ee88fa54608f4ce6f8cc7e9a1453ee
Task Blitz <taskblitzclick@gmail.com>
2025-11-04 12:42:45 +0100

    Remove SQL files - not needed for deployment

--- supabase-disable-rls.sql
+++
@@ -1,8 +0,0 @@
--- Temporarily disable RLS for testing
--- Run this in Supabase SQL Editor
-
--- Disable RLS on tasks table for testing
-ALTER TABLE tasks DISABLE ROW LEVEL SECURITY;
-
--- Also make sure we can read users table
-ALTER TABLE users DISABLE ROW LEVEL SECURITY;
